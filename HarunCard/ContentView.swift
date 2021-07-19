87861ab44ec1047ad761c9ce769d1f293192005b
Harun Aydın <harunaydin99@gmail.com>
2021-07-19 11:04:43 +0300

    Initial Commit

---
+++ HarunCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HarunCard
+//
+//  Created by Harun AYDIN on 19.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HarunCard/HarunCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarunCardApp.swift
+//  HarunCard
+//
+//  Created by Harun AYDIN on 19.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HarunCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
